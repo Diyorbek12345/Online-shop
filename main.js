add0b3d89f1ffce3a0cf9bf399f6b5517ef017bb
diyorbek1234567 <yusupovamatluba>
2023-09-18 17:11:51 +0500

    project has been continuing

---
+++ main.js
@@ -0,0 +1,20 @@
+export const cars = [
+  {
+    id:1,
+    name: "Gelik",
+    price: "350000$",
+    img: "./img/g63.avif"
+  },
+  {
+    id:2,
+    name: "Gelik",
+    price: "770000$",
+    img: "rollc_royce.jpg"
+  },
+  {
+    id:3,
+    name: "Cadilac",
+    price: "200000$",
+    img: "./cadilac.avif"
+  }
+]
